4d22fb22b02a2a1aa0114ed527e3484d4fed7ac2
redhendery <redhendery@gmail.com>
2016-07-26 15:45:46 +1200

    add about & contact pages and routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Alister Hendery' });
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact')
+});
+
+router.get('/about', (req, res) => {
+  res.render('about')
 });
 
 module.exports = router;
